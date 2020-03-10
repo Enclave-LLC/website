2c6182700fbcce4da24caf39463a7ad8177d1ad1
AmosAidoo <aidooamos20@gmail.com>
2020-03-10 16:19:12 +0000

    Entry point

---
+++ index.php
@@ -0,0 +1,111 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <?php include 'includes/head_items.php'?>
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <?php include 'includes/nav.php';?>
+    
+    <!--Header of Page-->
+    <div class="header-large bg-enclave">
+      <div class="container-sm text-white text-center">
+        <div class="row">
+          <div class="col-12">
+            <h1>Some Text</h1>
+            <h3 class="rw-words rw-words-1">
+              <span>breathtaking moments</span>
+              <span>lovely sounds</span>
+              <span>incredible magic</span>
+              <span>unseen experiences</span>
+              <span>happy feelings</span>
+              <span>beautiful butterflies</span>
+            </h3>
+            <div style="position: relative; margin-top: 50px">
+              <!--Google Play Image-->
+              <a href="#"><img alt="Google Play Badge" src="assets/images/google-play-badge.png" width="250"></a>
+            </div>
+            
+          </div>
+
+          <div class="col-12 text-center">
+            <!--Image of Phone displaying the app-->
+            <img class="img-fluid" alt="Phone displaying app" src="assets/images/samsung.png" width="350">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--Main Body of the Page-->
+    <!--Welcome Notice-->
+    <div class="bg-grey">
+      <div id="welcome" class="container-sm pt-md-5 pb-md-5">
+        <h1 class="text-enclave heading">Welcome to Enclave</h1>
+        <p class="paragraph">
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+          quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+          proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        </p>
+      </div>
+    </div>
+
+    <!--Features-->
+    <div id="features">
+      <div class="container-sm mt-5">
+        <h1 class="text-enclave heading">Features</h1>
+        <div class="row">
+          <!--Feature Description-->
+          <div class="col-sm-12 col-md-6">
+            <h2>Smooth Scrolling</h2>
+            <p class="paragraph">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+              tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+              quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+              consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+              cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+              proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+
+          <!--Feature Image-->
+          <div class="col-12 col-md-6">
+            <img class="img-fluid" alt="Feature Image" src="assets/images/dummy.png">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--Download the App-->
+    <div id="download" class="bg-enclave">
+      <div class="container-sm container-md container-lg pt-md-5 text-white">
+        <div class="row">
+          <div class="col-sm-12 col-md-6">
+            <!--Image of Phone displaying the app-->
+            <img class="img-fluid tilt-image" alt="Phone displaying app" src="assets/images/samsung.png" width="350">
+          </div>
+
+          <div class="col-sm-12 col-md-6 pt-md-5">
+            <h1>Download Enclave</h1>
+            <!--Google Play Image-->
+            <p>
+              Some long paragraph telling you why you should
+            </p>
+            <a href="#"><img alt="Google Play Badge" src="assets/images/google-play-badge.png" width="250"></a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!--Footer of Page-->
+    <?php include 'includes/footer.php'; ?>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  </body>
+</html>
